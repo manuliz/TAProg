ec9240370c40a26652b5c838bea6f628aa6f60bd
manuliz <m.liz@usal.edu.ar>
2020-07-24 16:38:46 -0300

    Se implementa metodos

---
+++ Aerolinea-DAO/src/edu/usal/negocio/DAO/Implementacion/LineasAereasDAOImplementacion.java
@@ -0,0 +1,40 @@
+package edu.usal.negocio.DAO.Implementacion;
+
+import java.util.List;
+
+import edu.usal.negocio.DAO.Interfaces.LineasAereasDAO;
+import edu.usal.negocio.Dominio.LineasAereas;
+
+public class LineasAereasDAOImplementacion implements LineasAereasDAO{
+
+	@Override
+	public LineasAereas obtenerLineaAerea(int idLineaAerea) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<LineasAereas> obtenerLineasAereas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void crearLineaAerea(LineasAereas lineasAereas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actualizarLineaAerea(LineasAereas lineasAereas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarLineaAerea(LineasAereas lineasAereas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
